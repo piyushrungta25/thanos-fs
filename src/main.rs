cd2c189fdde93c0c88f46e95d8c6a55a77372f94
Piyush Rungta <piyushrungta25@gmail.com>
2019-12-24 18:04:44 +0530

    Setup logging and unmount on termination

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+#[macro_use]
+extern crate log;
+
+use fuse::Filesystem;
+use std::env;
+use std::process::Command;
+
+struct NullFS;
+
+impl Filesystem for NullFS {}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    // let mountpoint = env::args_os().nth(1).unwrap();
+    let mountpoint = "/tmp/fuse";
+    debug!("register callback for sigterm");
+
+    ctrlc::set_handler(|| {
+        debug!("attempting unmount");
+        Command::new("fusermount")
+            .args(&["-u", "/tmp/fuse"])
+            .output()
+            .expect("error running unmount command");
+        debug!("unmount successful");
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    fuse::mount(NullFS, &mountpoint, &[]).unwrap();
 }
